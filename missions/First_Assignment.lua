109ae635f05b6f82ee1a4727f5368e3a1d118c28
rocket5115 <65498427+rocket5115@users.noreply.github.com>
2022-09-15 08:27:47 +0200

    Update First_Assignment.lua

--- missions/First_Assignment.lua
+++ missions/First_Assignment.lua
@@ -6,8 +6,11 @@ Missions.First_Assignment={
     }
 }
 function Missions.First_Assignment:Start(cb)
+    local obj = self
+    obj.CreateOnScreenMarker(150.0, -200.0, 31.0)
     cb()
 end
 function Missions.First_Assignment:End(cb)
+    local obj = self
     cb()
 end
